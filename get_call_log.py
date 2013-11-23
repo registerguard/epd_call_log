7a5225054ef6338c52339c212702976d6264d2ac
J. Heasly <jheasly@earthlink.net>
2013-11-22 19:52:34 -0800

    Got parsing down. Convert strings to date-time objs; insert.

--- get_call_log.py
+++ get_call_log.py
@@ -6,5 +6,61 @@ def main(backfill_date = ''):
     System/Computer Aided Dispatch system (RMS/CAD) used by Eugene and 
     Springfield, Ore., and insert into a Django application (not included).
     '''
+    
+    import datetime
+    import sys
+    import urllib
+    import urllib2
+    
+    from bs4 import BeautifulSoup
+    
+    if len(sys.argv) > 1:
+        my_eight_digit_date = sys.argv[1]
+        date_string = datetime.date(int(sys.argv[1][:4]), int(sys.argv[1][4:6]), int(sys.argv[1][6:])).strftime("%m/%d/%Y")
+    elif backfill_date:
+         date_string = backfill_date
+    else:
+        today = datetime.date.today()
+        my_eight_digit_date = today.strftime("%m/%d/%Y")
+        date_string = today.strftime("%m/%d/%Y")
+    
+    url = 'http://coeapps.eugene-or.gov/EPDDispatchLog/Search'
+    values = {
+        'DateFrom': date_string, 
+        'DateThrough': date_string, 
+        'IncidentType': '', 
+        'Disposition': '', 
+        'Priority': '', 
+        'EventNumberFilterOption': 'IsExactly', 
+        'EventNumber': '', 
+        'StreetNumberFilterOption': 'IsExactly', 
+        'StreetNumber': '', 
+        'StreetNameFilterOption': 'IsExactly', 
+        'StreetName': '', 
+        'CaseNumberFilterOption': 'IsExactly', 
+        'CaseNumber': '',
+    }
+    data = urllib.urlencode(values)
+    req = urllib2.Request(url, data)
+    response = urllib2.urlopen(req)
+    the_page = response.read()
+    
+    soup = BeautifulSoup(the_page)
+    
+    body = soup.findAll('tbody')[0]
+    rows = body.findAll('tr')
+    for tr in rows:
+        cols = tr.findAll('td')
+        call_time, dispatch_time, incident_desc, officers, disposition, event_number, location, priority, case = cols[0].string, cols[1].string, cols[2].string, cols[3].string, cols[4].string, cols[5].string, cols[6].string, cols[7].string, cols[8].string
+        
+        print '''call_time: %s
+        dispatch_time: %s
+        incident_desc: %s 
+        officers: %s 
+        disposition: %s 
+        event_number: %s 
+        location: %s 
+        priority: %s 
+        case: %s ''' % (call_time, dispatch_time, incident_desc, officers, disposition, event_number, location, priority, case)
 
 if __name__ == "__main__" : main()
